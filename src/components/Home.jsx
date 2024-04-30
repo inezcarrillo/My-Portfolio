f18b82dd5e772dc2a32606a4c183d81c6f41a02f
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-29 23:05:59 -0500

    created the home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>
+                Hey! Welcome to my portfolio
+            </p>
+        </div>
+    )
+};
