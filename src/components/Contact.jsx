30a7798f65ce9f7aa356eac4e9be07822fa1001c
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-30 16:43:52 -0500

    corrected my email and github

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,11 @@ export default function Contact() {
     return (
         <div className="contact">
         <h2>Contact</h2>
-        <p>If you'd like to get in touch with me, feel free to reach out via email or through my GitHub:</p>
+        <p>If you would like to get in touch with me, feel free to reach out via email or through my GitHub:</p>
         <ul>
-          <li>Email: <a href="mailto:inezcarrillo42@gmail.com">your.email@example.com</a></li>
-          <li>GitHub: <a href="https://github.com/inezcarrillo">github.com/yourusername</a></li>
+          <li>Email: <a href="mailto:inezcarrillo42@gmail.com">inezcarrillo42@gmail.com</a></li>
+          <li>GitHub: <a href="https://github.com/inezcarrillo">github.com/inezcarrillo</a></li>
         </ul>
       </div>
     )
-};
+}
