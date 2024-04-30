5aef38b0c737959d379c2fab6e3dc0f5cfb3b78d
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-29 22:56:50 -0500

    created contacts for contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+        <div className="contact">
+        <h2>Contact</h2>
+        <p>If you'd like to get in touch with me, feel free to reach out via email or through my GitHub:</p>
+        <ul>
+          <li>Email: <a href="mailto:inezcarrillo42@gmail.com">your.email@example.com</a></li>
+          <li>GitHub: <a href="https://github.com/inezcarrillo">github.com/yourusername</a></li>
+        </ul>
+      </div>
+    )
+};
