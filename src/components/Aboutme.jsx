989b25b6f17aa131d0f4df865e849e86c30a6748
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-01 18:20:24 -0500

    added to aboutme paragraph

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,8 +3,10 @@ export default function AboutMe() {
         <div>
             <h1>About Me</h1>
             <p>
-                Hi there! My name is Inez Carrillo. I am a first generation Mexican American. I currently work in health care in the radiology department, nuclear medician to be exact. I have loved working in health care but know there is much more out there for me. Learning software development has been challenging but very rewarding. I hope to grow and become better everyday.
+                Hi there! My name is Inez Carrillo. I am a first generation Mexican American. I currently work in health care in the radiology department, nuclear medicine to be exact. I have loved working in health care but know there is much more out there for me. Learning software development has been challenging but very rewarding. I hope to grow and become better everyday.
+                I am a huge family person. Since moving away from home 3 years ago, I still try to maintain a close connection with my family. 
+                Some things I love doing on my free time are running 5k, working out, hanging out with friends, going to concerts, and trying new restaurants.          
             </p>
         </div>
     )
-};
+}
